7527840375d1abd686c891a93704cae7603323df
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-08-30 12:29:18 +0300

    add categories

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ function App() {
   return (
     <div className="App">
       <Landing/>
+      <div className='categories'>
+        <div className="category all">
+          <p>all in one</p>
+        </div>
+        <div className="category indian">
+          <p>indian</p>
+        </div>
+        <div className="category vegetarian">
+          <p>vegetarian</p>
+        </div>
+        <div className="category healthy">
+          <p>healthy</p>
+        </div>
+      </div>
     </div>
   );
 }
