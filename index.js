210d0433efd4a122fee753a82cc3bc000f2560ae
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-09-13 12:52:05 +0300

    make search component and landing page responsive

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
 const { response } = require('express')
-const fetch = require('node-fetch');
+
 
 
 require('dotenv').config()
@@ -15,22 +15,8 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/list', (req,res)=>{
-  const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '6b04fbf685msh8d00e3cc6729138p1e5afbjsn02ffeb8dbf46',
-      'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    }
-  };
-
-  fetch(url, options)
-    .then(res => res.json())
-    .then(json => res.json(json))
-    .catch(err => ('error:' + err));
-
-
+  res.json('hi')
 })
 
 app.listen(8000, () => console.log(`Server is running on port ${PORT}`))
